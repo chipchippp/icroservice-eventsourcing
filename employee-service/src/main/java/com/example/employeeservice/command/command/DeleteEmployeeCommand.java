3c1d904a1145bb83cd2435170e5c336816d34972
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-10-19 14:07:09 +0700

    done delete employee

---
+++ employee-service/src/main/java/com/example/employeeservice/command/command/DeleteEmployeeCommand.java
@@ -0,0 +1,14 @@
+package com.example.employeeservice.command.command;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteEmployeeCommand {
+    @TargetAggregateIdentifier
+    private String id;
+}
--- employee-service/src/main/java/com/example/employeeservice/command/event/EmployeeEventHandler.java
+++ employee-service/src/main/java/com/example/employeeservice/command/event/EmployeeEventHandler.java
@@ -3,12 +3,15 @@ package com.example.employeeservice.command.event;
 import com.example.employeeservice.command.data.Employee;
 import com.example.employeeservice.command.data.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.DisallowReplay;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class EmployeeEventHandler {
@@ -32,11 +35,13 @@ public class EmployeeEventHandler {
     }
 
     @EventHandler
+    @DisallowReplay
     public void on(EmployeeDeletedEvent event) {
-        Optional<Employee> oldEmployee = employeeRepository.findById(event.getId());
-
-        oldEmployee.ifPresent(employee -> {
-            employeeRepository.delete(employee);
-        });
+        try {
+            employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee not found"));
+            employeeRepository.deleteById(event.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
